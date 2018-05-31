09dda2450b596839047844293895321558e5545f
Sander <pilot008@gmail.com>
2018-05-31 11:52:50 +0200

    Less pages

--- lib/Users.py
+++ lib/Users.py
@@ -283,8 +283,9 @@ class Users:
 				if "Director" in baseroles:
 					page = 1
 					hasMorePages = True
-					while hasMorePages or page < 20:
+					while hasMorePages or page < 10:
 						assetList = self.esi.getESIInfo('get_corporations_corporation_id_assets', {"corporation_id": corpID, "page": page})
+						print(assetList)
 						for asset in assetList:
 							if asset["location_id"] < 69999999:
 								itemList.add(asset["location_id"])
