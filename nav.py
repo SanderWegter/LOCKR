8016e1d3e4583da7c638db990a7d6aad49e5f09d
Sander <pilot008@gmail.com>
2018-05-31 10:32:25 +0200

    Added corpassets (admin only)

--- nav.py
+++ nav.py
@@ -1,5 +1,6 @@
 navigation_bar = [
 	# href, 	id,        	logo,         			caption, 		isAdmin
     ('/', 		'index', 	'fa fa-dashboard', 		'Dashboard', 	False),
+    ('/assets', 'assets', 	'fa fa-archive', 		'CorpAssets', 	False),
     ('/admin', 	'admin', 	'fa fa-space-shuttle', 	'Admin', 		True)
 ]
