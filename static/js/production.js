f944ab3a2bb8b00907a9e178b9752be77152fb6c
Sander <pilot008@gmail.com>
2018-06-08 17:39:10 +0200

    Added maxproducable

--- static/js/production.js
+++ static/js/production.js
@@ -2,19 +2,23 @@ function getProduction(){
     $.getJSON("/internal/production/getProduction", function(data){
         $.each(data.production, function(k,v){
             var inputMaterials = "<table><tr>"
+            maxproducable = []
             $.each(v, function(key,val){
                 if (val.stock >= val.quantity){
                     stockquan = "<font color='green'>"+val.stock+"/"+val.quantity+"</font>"
+                    maxproducable.append(Math.floor(val.stock / val.quantity))
+                    producable = "<br>"+Math.floor(val.stock / val.quantity)
                 }
                 else{
                     stockquan = "<font color='red'>"+val.stock+"/"+val.quantity+"</font>"
+                    producable = ""
                 }
-                inputMaterials += "<td><img src='https://image.eveonline.com/Type/"+key+"_32.png'><br>"+data.translations[key].name + "<br>"+stockquan+"</td>"
+                inputMaterials += "<td><img src='https://image.eveonline.com/Type/"+key+"_32.png'><br>"+data.translations[key].name + "<br>"+stockquan+""+producable+"</td>"
             })
             inputMaterials += "</tr></table>"
             $(".productionList").append("\
                                             <tr>\
-                                            <td><img src='https://image.eveonline.com/Type/"+k+"_32.png'><br>"+data.translations[k].name+"</td>\
+                                            <td><img src='https://image.eveonline.com/Type/"+k+"_32.png'><br>"+data.translations[k].name+"<br>"+Math.max(...maxproducable)+"</td>\
                                             <td>"+inputMaterials+"</td>\
                                             </tr>\
             ")
