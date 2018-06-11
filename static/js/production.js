37c33a2d2140794d5414f01c26546aeeea2bfc78
Sander <pilot008@gmail.com>
2018-06-11 16:08:46 +0200

    Added counting items in build

--- static/js/production.js
+++ static/js/production.js
@@ -3,6 +3,7 @@ function getProduction(){
         $.each(data.production, function(k,v){
             var inputMaterials = "<table><tr>"
             maxproducable = []
+            maxproducablebuild = []
             $.each(v, function(key,val){
                 if (val.stock >= val.quantity){
                     stockquan = "<font color='green'>"+val.stock+"/"+val.quantity+"</font>"
@@ -14,12 +15,19 @@ function getProduction(){
                     maxproducable.push(0)
                     producable = ""
                 }
+
+                inprod = ""
+                if (val.inbuild > 0){
+                    stockquan += "("+val.inbuild+")"
+                    maxproducablebuild.push(Math.floor((val.stock + val.inbuild / val.quantity)))
+                }
+
                 inputMaterials += "<td><img src='https://image.eveonline.com/Type/"+key+"_32.png'><br>"+data.translations[key].name + "<br>"+stockquan+""+producable+"</td>"
             })
             inputMaterials += "</tr></table>"
             $(".productionList").append("\
                                             <tr>\
-                                            <td><img src='https://image.eveonline.com/Type/"+k+"_32.png'><br>"+data.translations[k].name+"<br>"+Math.min(...maxproducable)+"</td>\
+                                            <td><img src='https://image.eveonline.com/Type/"+k+"_32.png'><br>"+data.translations[k].name+"<br>"+Math.min(...maxproducable)+"("+Math.min(...maxproducablebuild)+")</td>\
                                             <td>"+inputMaterials+"</td>\
                                             </tr>\
             ")
