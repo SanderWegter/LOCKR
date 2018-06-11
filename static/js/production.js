bd9592e358ac1210800aaea878c8d4e9d7318a36
Sander <pilot008@gmail.com>
2018-06-11 16:11:10 +0200

    Prettified

--- static/js/production.js
+++ static/js/production.js
@@ -5,21 +5,23 @@ function getProduction(){
             maxproducable = []
             maxproducablebuild = []
             $.each(v, function(key,val){
+                clr = "green"
                 if (val.stock >= val.quantity){
-                    stockquan = "<font color='green'>"+val.stock+"/"+val.quantity+"</font>"
+                    stockquan = "<font color='"+clr+"'>"+val.stock+"/"+val.quantity+"</font>"
                     maxproducable.push(Math.floor(val.stock / val.quantity))
                     producable = "<br>"+Math.floor(val.stock / val.quantity)
                 }
                 else{
-                    stockquan = "<font color='red'>"+val.stock+"/"+val.quantity+"</font>"
+                    clr = "red"
+                    stockquan = "<font color='"+clr+"'>"+val.stock+"/"+val.quantity+"</font>"
                     maxproducable.push(0)
                     producable = ""
                 }
 
                 inprod = ""
                 if (val.inbuild > 0){
-                    stockquan += "("+val.inbuild+")"
-                    maxproducablebuild.push(Math.floor((val.stock + val.inbuild / val.quantity)))
+                    stockquan = "<font color='"+clr+"'>"+val.stock+"(+"+val.inbuild+")/"+val.quantity+"</font>"
+                    maxproducablebuild.push(Math.floor((val.stock + val.inbuild) / val.quantity))
                 }
 
                 inputMaterials += "<td><img src='https://image.eveonline.com/Type/"+key+"_32.png'><br>"+data.translations[key].name + "<br>"+stockquan+""+producable+"</td>"
