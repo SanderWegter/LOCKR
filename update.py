14e12188bb967dfc5e536487af7076f28ded10c4
Sander <pilot008@gmail.com>
2018-07-04 16:07:45 +0200

    Testing update

--- update.py
+++ update.py
@@ -378,7 +378,10 @@ class Functions:
             items = ",".join(items)
 
             r = get(baseURL+items)
+            print(r.status_code)
+            print(r.text)
             marketInfo = r.json()
+            
             for r in marketInfo:
                 print(r)
                 typeID = r["buy"]["forQuery"]["types"][0]
